9b663e9a5f3b442bf077a4cbce49127b157abe8f
Rickkless-King <rickklessjiayou@gmail.com>
2025-10-19 20:53:25 +0800

    新增获取股票盈利日历、特定时间段财报数据、公司基本财务信息、内部人士交易及情绪的函数，并更新README文档以反映新功能

--- Agents/fundamental_analyst.py
+++ Agents/fundamental_analyst.py
@@ -109,10 +109,173 @@ def get_company_peers(symbol):
     company_peers=finnhub_client.company_peers(symbol)
     return company_peers
 
-print(get_company_peers('NVDA'))
+# print(get_company_peers('NVDA'))
 
 
-# 定义获取财报发布时间、实际业绩、市场预期数据以及对应季度和年份的函数  (免费版只能获取一个月的历史盈利数据及新更新)
+# 定义获取指定股票盈利日历(财报发布时间、实际业绩、市场预期数据以及对应季度和年份)的函数  (免费版只能获取最近一个月发布财报的公司的数据——两个限定：最新一个月+发布财报的公司)
+def get_earnings_calendar(str_time,end_time,symbol):
+    earnings_calendar=finnhub_client.earnings_calendar(_from=str_time,to=end_time,symbol=symbol,international=False)
+    report_list=earnings_calendar.get('earningsCalendar',[])#获取最外面字典里面的列表
+    report=report_list[0]#上面代码获取的report_list是一个列表，不能直接.get()方法，因此需要进入到列表里面，里面才是字典
+    
+
+    # return{ # 如果你是免费版且没有遵循 最新一个月+发布财报的公司 两个要求，就会返回一个空列表[]
+    #     "股票代码":earnings_calendar.get('symbol'),
+    #     "报告区间":f"{earnings_calendar.get('year')}年第{earnings_calendar.get('quarter')}季",
+    #     "财报发布时间":earnings_calendar.get('date'),
+    #     "预计营收":earnings_calendar.get('revenueEstimate'),
+    #     "实际营收":earnings_calendar.get('revenueActual'),
+    #     "预期EPS":earnings_calendar.get('epsEstimate'),
+    #     "实际EPS":earnings_calendar.get('epsActual'),
+    #     "发布时间":earnings_calendar.get('hour')
+    # } 现在上面的代码打印都返回None，AI大模型告诉我Finnhub盈利日历返回的是包含列表的字典，结构为{“earnings_calendar”：[{},{},....{}]},value里面的value才是单条财务报表数据
+    # 我上面的代码直接对最外面的字典做get操作，但实际都在列表里面的子字典中，因此全部返回None。
+
+    return{
+        "股票代码":report.get('symbol'),
+        "报告区间":f"{report.get('year')}年第{report.get('quarter')}季",
+        "财报发布时间":report.get('date'),
+        "预计营收(单位；亿美元)":report.get('revenueEstimate')/100000000,
+        "实际营收(单位；亿美元)":report.get('revenueActual')/100000000,
+        "预期EPS":report.get('epsEstimate'),
+        "实际EPS":report.get('epsActual'),
+        "发布时间":report.get('hour') 
+    }
+# print(get_earnings_calendar("2025-09-19","2025-10-19","BAC"))#笔者实际写作时间为2025年10月19日，因此打印最新刚发布财报的美国银行。
+
+
+
+# 定义获取区间段发布财报数据的函数(免费版查询最近一个月)
+# 刚才我制指定了symbol为BOA.us，在官方给出的范例中，symbol可以=""，然后返回某个时间区间内全部发布财报资料的股票
+def get_specific_time_period_earings_calendar(str_time,end_time):
+    specific_time_period_earings_calendar=finnhub_client.earnings_calendar(_from=str_time,to=end_time,symbol="",international=False)
+    # 上述代码不指定特定时间区间内的股票，因此返回的大字典里面为 "earningsCalendar"：[{字典1}，{字典2},...]
+    report_list=specific_time_period_earings_calendar.get("earningsCalendar",[])# get方法默认返回None，report_list为None后续做遍历的话会报错TypeError
+    all_reports=[]
+    for i in range(len(report_list)):
+        report=report_list[i]
+        formatted_report={
+        "股票代码":report.get('symbol'),
+        "报告区间":f"{report.get('year')}年第{report.get('quarter')}季",
+        "财报发布时间":report.get('date'),
+        "预计营收(单位；美元)":report.get('revenueEstimate'),#有些股票不知道为什么，预计营收/实际应收Finnhub返回都是None，因此不能像上面那样直接除
+        "实际营收(单位；美元)":report.get('revenueActual'),
+        "预期EPS":report.get('epsEstimate'),
+        "实际EPS":report.get('epsActual'),
+        "发布时间":report.get('hour') 
+        }
+        all_reports.append(formatted_report)
+    return all_reports #返回所有财报的解析结果
+
+# print(get_specific_time_period_earings_calendar("2025-10-01","2025-10-18"))
+
+
+# 定义获取公司基本财务信息(比如PE、PS、52周最高/低价、流动比率、净利率、美股营收额)的函数
+# 优化一下我写的内容：比如一些.get()方法应当返回一个默认空字典{}，同时针对series/annual这种多层嵌套字典，尝试.get().get()方法
+def get_company_basic_financials(symbol_1,symbol_2):
+    company_basic_financials=finnhub_client.company_basic_financials(symbol_1,symbol_2)
+    company_basic_financials_metricType=company_basic_financials.get("metricType")
+    company_basic_financials_symbol=company_basic_financials.get("symbol")
+    company_basic_financials_metric=company_basic_financials.get("metric",{})#通过.get()方法获取“metric”字典的value值
+
+    company_basic_financials_serires=company_basic_financials.get("series")# 通过.get()方法获取一系列基础财务数据,返回的是“annual”字典的value值
+
+    company_annual_currentRatio=company_basic_financials_serires.get("annual",{}).get("currentRatio",[])
+    company_annual_salesPerShare=company_basic_financials_serires.get("annual",{}).get("salesPerShare",[])
+    company_annual_netMargin=company_basic_financials_serires.get("annual",{}).get("netMargin",[])
+    return{
+        "公司名":company_basic_financials_symbol,
+        "本次输出数据范围":company_basic_financials_metricType,
+        "最近10个交易日平均交易量":company_basic_financials_metric.get("10DayAverageTradingVolume"),
+        "52周最高":company_basic_financials_metric.get("52WeekHigh"),
+        "52周最低":company_basic_financials_metric.get("52WeekLow"),
+        "52周内最低点发生日期":company_basic_financials_metric.get("52WeekLowDate"),
+        "基于过去52周每日收盘价计算的价格回报率":company_basic_financials_metric.get("52WeekPriceReturnDaily"),
+        "Beta系数":company_basic_financials_metric.get("beta"),
+        "流动比率历史变化情况":company_annual_currentRatio,
+        "每股对应销售额历史变化情况":company_annual_salesPerShare,
+        "净利润率历史变化情况":company_annual_netMargin
+    }
+
+# print(get_company_basic_financials('NVDA','all'))
+
+# 定义获取公司内部人士增/减持股票的函数(每次调用最多不能超过100笔交易)
+def get_stock_inside_transactions(symbol,str_time,end_time):
+    stock_inside_transactions=finnhub_client.stock_insider_transactions(symbol,str_time,end_time)
+    all_inside_transactions_data=[]
+    for i in range(len(stock_inside_transactions.get("data"))):
+        inside_transactions_data={
+            "减持的股票为":stock_inside_transactions.get("symbol"),
+            "减持者姓名":stock_inside_transactions.get("data")[i].get("name"),
+            "交易后持有的股份":stock_inside_transactions.get("data")[i].get("share"),
+            "相较于上次披露，股份变化(正数表示增持，负数表示减持)":stock_inside_transactions.get("data")[i].get("change"),
+            "提交日期":stock_inside_transactions.get("data")[i].get("filingDate"),
+            "交易日期":stock_inside_transactions.get("data")[i].get("transactionDate"),
+            "交易代码":stock_inside_transactions.get("data")[i].get("transactionCode"),
+            "平均交易价格":stock_inside_transactions.get("data")[i].get("transactionPrice"),
+        }
+        all_inside_transactions_data.append(inside_transactions_data)
+    return all_inside_transactions_data
+
+# print(get_stock_inside_transactions("NVDA",'2025-09-19','2025-10-19'))
+
+# 定义获取公司内部人士情绪的函数(其中MSPR为Finnhub独创的指标，范围从-100极度看空到100极度看多)
+# 但Finnhub自己也说了，预示未来30-90天的价格变化，没说长期。
+def get_stock_insider_sentiment(symbol,str_time,end_time):
+    stock_insider_sentiment=finnhub_client.stock_insider_sentiment(symbol,str_time,end_time)
+    all_insider_sentiment=[]
+    for i in range(len(stock_insider_sentiment.get("data"))):
+        insider_sentiment={
+            "股票代码":stock_insider_sentiment.get("data")[i].get("symbol"),
+            "时间":f'{stock_insider_sentiment.get("data")[i].get("year")}年{stock_insider_sentiment.get("data")[i].get("month")}月',#使用f-string嵌套的时候，需要注意“ ‘的问题
+            "所有内部人员交易合计的净买入/卖出":stock_insider_sentiment.get("data")[i].get("change"),
+            "月度股票购买比例":stock_insider_sentiment.get("data")[i].get("mspr"),
+        }
+        all_insider_sentiment.append(insider_sentiment)
+    return all_insider_sentiment
+
+# print(get_stock_insider_sentiment('NVDA','2024-01-01','2024-12-31'))#有些月份有，有些月份没有，以2024年-12月为例，NVDA只有1-8月的相关数据
+
+# 获取已报告的财务数据
+def get_financials_reported(symbol,freq_time):
+    financials_reported=finnhub_client.financials_reported(symbol=symbol,freq=freq_time)
+    financial_reported_details={
+        "股票代码":financials_reported.get("symbol"),
+        "对应的CIK编码为":financials_reported.get("cik"),
+        "具体的财务数据如下":financials_reported.get("data")
+    }
+    return financial_reported_details
+# print(get_financials_reported('NVDA','annual'))
+
+
+# 获取上市公司在SEC备案文件(一次最多不超过250份)
+def get_stock_filings(symbol,str_time,end_time):
+    stock_filings=finnhub_client.filings(symbol=symbol,_from=str_time,to=end_time)
+    return stock_filings
+# print(get_stock_filings('NVDA','2025-09-01','2025-10-01'))
+
+    
+
+
+
+
+        
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -2,9 +2,24 @@
 An AI investment assistant based on a large language model
 
 
+### V1.1版本-2025年10月19日更新
+#### 添加了日常在做基本面分析的时候可能会用到的一些数据指标
+包括：
+1. 公司概况get_company_profile()
+2. 获取实时股票数据报价get_real_time_data()  PS:免费版有15-30分钟的延迟
+3. 获取有关公司新闻get_company_news()  PS:免费版限制最近30天
+4. 获取同行业其他公司列表get_company_peers()
+5. 获取指定股票盈利日历(财报发布时间、实际业绩、市场预期数据以及对应季度和年份)get_earnings_calendar()  PS:这里可以指定比如说'NVDA'
+6. 获取指定区间段发布财报的公司相关数据get_specific_time_period_earings_calendar()  PS：跟上面类似
+7. 获取公司基本财务信息(比如PE、PS、52周最高/低价、流动比率、净利率、营收额) get_company_basic_financials()
+8. 获取公司内部人士增/减持股票的相关信息get_stock_inside_transactions()
+9. 获取公司内部人士情绪的相关信息get_stock_insider_sentiment()
+10. 获取已报告的财务数据(包括财务报表) get_financials_reported()
+11. 上市公司在SEC备案文件(一次不能超过250份)get_stock_filings()
+目前我是通过定义获取函数的方式，并将得到的返回结果打印出来。
 
-## V 1.0版本——2025年10月18日正式开始
-### 如何从0开始创建一个项目
+### V1.0版本——2025年10月18日正式开始
+#### 如何从0开始创建一个项目
 1. 首先在Github上点击【new】，首先给仓库起一个名字，注意Github会检测这个名字是否可用
 2. 然后添加描述→选择仓库是公开or私人→是否添加README.md文件→是否添加.gitignore→是否添加License，这里会有一堆License让你选择，但一般选MIT或者Apache
 3. 创建成功后，然后把仓库克隆到本地某个文件里，建议使用Git克隆到指定为止
